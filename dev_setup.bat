785132cf63b63d2e66e1aae79d2b9ad928813153
Silas Bischoff <silas.bischoff@brockhaus-ag.de>
2018-05-13 23:15:15 +0200

    Fixed venv-setup-script and requirement handling

--- dev_setup.bat
+++ dev_setup.bat
@@ -3,28 +3,26 @@ ECHO Only run this once!
 ECHO Create venv, add path to PYTHONPATH and install dependencies?
 SET /p continue=[y/n] 
 IF %continue%==n EXIT
-SET /p path=Input path to Python Distribution (with backslashes): 
+ECHO Input path to Python Distribution (directory containing python.exe)
+SET /p path=(without slash at the end): 
 ECHO ##################################
 ECHO Creating Virtual Environment ...
 ECHO ##################################
 MKDIR %~dp0venv
-CALL %path%\Scripts\activate.bat
-ECHO python activated, creating venv ...
-python -m venv %~dp0venv
+%path%\python.exe -m venv %~dp0venv
 ECHO ##################################
 ECHO Activating Virtual Environment ...
 ECHO ##################################
-CALL %~dp0venv\Scripts\activate.bat
 ECHO Appending Development Directory to PYTHONPATH ...
 > %~dp0venv\Lib\site-packages\pnd.pth ECHO %~dp0
 ECHO ##################################
 ECHO Installing Dependencies ...
 ECHO ##################################
-python -m pip install --upgrade pip
-pip install pytest
-pip install appdirs
+%~dp0venv\Scripts\python.exe -m pip install --upgrade pip
+%~dp0venv\Scripts\python.exe -m pip install -r %~dp0requirements.txt
 ECHO ##################################
 ECHO Dev Setup completed.
-ECHO Use .\venv environment for testing
+ECHO Use .\venv environment for testing.
+ECHO To start, execute 'python -m pnd' in activated environment
 ECHO ##################################
 PAUSE
